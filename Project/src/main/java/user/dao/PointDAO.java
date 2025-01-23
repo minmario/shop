5ef5cb7264cb80cc13a89578726746556fcb49f1
knh1721 <rnjsskgus2@gmail.com>
2025-01-23 23:35:15 +0900

    임시 커밋

--- Project/src/main/java/user/dao/PointDAO.java
+++ Project/src/main/java/user/dao/PointDAO.java
@@ -8,12 +8,12 @@ import java.util.List;
 
 public class PointDAO {
     // 적립금 내역 가져오기
-    public static List<PointVO> getPoint(String cusno) {
+    public static List<PointVO> getPoint(String cus_no) {
         List<PointVO> points = null;
         SqlSession ss = FactoryService.getFactory().openSession();
 
         try {
-            points = ss.selectList("point.select_point", cusno);
+            points = ss.selectList("point.select_point", cus_no);
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
