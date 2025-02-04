39a17d4d04480860ad70ee32b9cd9e0a7d37a2b3
000flag <vili1265@naver.com>
2025-02-05 02:10:21 +0900

    주문 완료 페이지 작업 중(임시 커밋)

--- Project/src/main/java/user/action/customer/OrderDetailsAction.java
+++ Project/src/main/java/user/action/customer/OrderDetailsAction.java
@@ -47,7 +47,7 @@ public class OrderDetailsAction implements Action {
                     request.setAttribute("totalPrice", totalPrice);
                     request.setAttribute("point_amount", point_amount);
 
-                    viewPage = "/user/customer/jsp/mypage/completedDetails.jsp";
+                    viewPage = "/user/customer/jsp/mypage/orderDetails.jsp";
                     break;
                 case "update":
                     String prev_deli_no = request.getParameter("pre_delivery_id");
