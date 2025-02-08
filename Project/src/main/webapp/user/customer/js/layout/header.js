43dbd7285a9b52caaaa0d296dbf1f377646947e5
000flag <vili1265@naver.com>
2025-02-08 18:02:55 +0900

    소스 정리

--- Project/src/main/webapp/user/customer/js/layout/header.js
+++ Project/src/main/webapp/user/customer/js/layout/header.js
@@ -1,10 +1,3 @@
-document.addEventListener('DOMContentLoaded', function () {
-    if (performance.navigation.type === 2) { // 뒤로 가기 탐지
-        alert("뒤로가기");
-        location.reload(); // 강제 새로고침
-    }
-});
-
 function onClickLogout() {
     window.location.replace("Controller?type=logout");
 }
